be4b5e377b2520682ab664284376eb10d93742d6
xianxianss <75553503+xianxianss@users.noreply.github.com>
2021-02-07 01:23:47 +0800

    攝影機追蹤和左右移動

--- RPGHorrible/Assets/Scripts/CameraCControl.cs
+++ RPGHorrible/Assets/Scripts/CameraCControl.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraCControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("目標物件")]
+    public Transform target;
+    
+    [Header("速度"), Range(0, 1000)]
+    public float speed = 1.5f;
+
+    
+    /// <summary>
+    /// 追蹤目標物件
+    /// </summary>
+    private void Track()
     {
-        
+        Vector3 posA = target.position;
+        Vector3 posB = transform.position;
+        posA.z = -10;
+        posB = Vector3.Lerp(posA, posB, 0.5f * speed * Time.deltaTime);
+        transform.position = posB;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        Track();
     }
+
+
 }
--- RPGHorrible/Assets/Scripts/Player.cs
+++ RPGHorrible/Assets/Scripts/Player.cs
@@ -1,18 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region 欄位
+    [Header("移動速度"), Range(0, 1000)]
+    public float speed;
+
+    public float h;
+    private Rigidbody2D rig;
+
+    #endregion
+
+    private void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+        GetHorizontal();
     }
-}
+
+
+    #region 方法
+
+    private void GetHorizontal()
+    {
+        h = Input.GetAxis("Horizontal");
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()
+    {
+        rig.velocity = new Vector2(h * speed, rig.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.localEulerAngles = Vector3.zero;
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.localEulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+
+
+
+
+
+        #endregion
+    }
